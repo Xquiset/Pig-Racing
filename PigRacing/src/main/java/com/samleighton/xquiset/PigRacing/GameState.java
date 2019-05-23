c4bd5de24a882e50521f7a78fcf0509005570cb6
Xquiset <snowsam99@aol.com>
2019-05-22 17:14:11 -0700

    Added GameState functionality

--- PigRacing/src/main/java/com/samleighton/xquiset/PigRacing/GameState.java
+++ PigRacing/src/main/java/com/samleighton/xquiset/PigRacing/GameState.java
@@ -1,5 +1,29 @@
 package com.samleighton.xquiset.PigRacing;
 
 public enum GameState {
-
+	IN_LOBBY(true), IN_GAME(false), FINISHED(false), RESETTING(false);
+	
+	private boolean joinable;
+	
+	private static GameState currentState;
+	
+	GameState(boolean joinable) {
+		this.joinable = joinable;
+	}
+	
+	public boolean isJoinable() {
+		return this.joinable;
+	}
+	
+	public static boolean isState(GameState state) {
+		return GameState.currentState == state;
+	}
+	
+	public static void setState(GameState state) {
+		currentState = state;
+	}
+	
+	public static GameState getState() {
+		return currentState;
+	}
 }
